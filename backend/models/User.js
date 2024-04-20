1e402ea594114d700b95dcf6180c697cdaa72e5c
오홍석 <lkl4502@kookmin.ac.kr>
2024-04-20 22:51:41 +0900

    User Table 추가

---
+++ backend/models/User.js
@@ -0,0 +1,52 @@
+const { DataTypes } = require("sequelize");
+const { sequelize } = require(".");
+
+module.exports = (sequelize, DataTypes) => {
+  const User = sequelize.define(
+    "User",
+    {
+      email: {
+        type: DataTypes.STRING(20),
+        primaryKey: true,
+        comment: "사용자 Email",
+      },
+      name: {
+        type: DataTypes.STRING(10),
+        allowNull: false,
+        comment: "사용자 이름",
+      },
+      age: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        comment: "사용자 나이",
+      },
+      sex: {
+        type: DataTypes.STRING(10),
+        allowNull: false,
+        comment: "사용자 성별",
+      },
+      phone: {
+        type: DataTypes.STRING(20),
+        allowNull: false,
+        comment: "사용자 휴대폰 번호",
+      },
+      password: {
+        type: DataTypes.STRING(30),
+        allowNull: false,
+        comment: "사용자 비밀번호",
+      },
+    },
+    {
+      charset: "utf8", // 한국어 설정
+      collate: "utf8_general_ci", // 한국어 설정
+      tableName: "User", // 테이블 이름
+    }
+  );
+
+  // User.associate = function (models) {
+  //   models.User.hasMany(models);
+  // };
+
+  return User;
+};
+// - 비밀번호
